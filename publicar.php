aac4544f6670621503ce40ac70d4ea28a75b9d11
root <root@fernando-compu.(none)>
2014-11-21 01:39:27 -0200

    a

--- publicar.php
+++ publicar.php
@@ -2,5 +2,6 @@
 require_once('post.php'); 
 $mensaje = 'Un wallpaper http://fernandomarichal.com/api/upload_facebook/ben10.jpg';
 $un_post = new Post($mensaje);
+var_dump($un_post);
 $ret = $un_post->publicarEnFacebook();
 var_dump($ret);
