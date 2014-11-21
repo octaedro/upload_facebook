632060bbe680826057f5fdffc3bf2b9feea56ffc
root <root@fernando-compu.(none)>
2014-11-21 11:05:39 -0200

    asdf

--- post.php
+++ post.php
@@ -1,22 +1,22 @@
 <?php
 require_once('facebook-php-sdk-master/src/facebook.php'); 
-
+//
 class Post{
     var $facebook;
     var $mensaje;
 
-    function __construct($mensaje='') {    
+    function __construct($secret,$mensaje='') {    
         $this->facebook = new Facebook(array(
                 'appId' => '746062735448615',
-                'secret' => '2af9092b09fe697e006cbbbd965c202d',
+                'secret' => $secret,
                 'cookie' => false
         ));
         $this->mensaje=$mensaje;
     }
 
-    public function publicarEnFacebook(){
+    public function publicarEnFacebook($token){
         $req =  array(
-                'access_token' => 'e15a5de77332a2b7e0497f97a40e0199',
+                'access_token' => $token,
                 'message' => $this->mensaje);
 
         $res = $this->facebook->api('/359335437580378/feed', 'POST', $req);
--- publicar.php
+++ publicar.php
@@ -1,7 +1,10 @@
 <?php
+//
 require_once('post.php'); 
+$secret = $_GET['secret'];
+$token = $_GET['token'];
 $mensaje = 'Un wallpaper http://fernandomarichal.com/api/upload_facebook/ben10.jpg';
-$un_post = new Post($mensaje);
+$un_post = new Post($secret,$mensaje);
 //var_dump($un_post);
 $ret = $un_post->publicarEnFacebook();
 var_dump($ret);
